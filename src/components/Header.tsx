44f61f1476ea0274a1de45b5557919e99cd6809b
Diogo Camacho <diogocamacho10@msn.com>
2025-02-05 08:48:15 -0300

    feat: criando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,24 @@
 import { FC, PropsWithChildren } from 'react';
 
-type HeaderType = PropsWithChildren<{ color: string }>;
+type HeaderType = PropsWithChildren<{
+  color: string;
+  image: { src: string; alt: string };
+}>;
 
-const Header: FC<HeaderType> = ({ children, color }) => {
-  return <h2 style={{ color }}>{children}</h2>;
+const Header: FC<HeaderType> = ({ children, color, image }) => {
+  return (
+    <header>
+      <img className='logo' alt={image.alt} src={image.src} />
+      <h2 style={{ color }} className='title'>
+        {children}
+      </h2>
+      <div className='buttons'>
+        <button>Home</button>
+        <button>Alerts</button>
+        <button>Settings</button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
